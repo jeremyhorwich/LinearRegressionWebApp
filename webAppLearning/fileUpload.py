769f9c33044436148c76fac9afb76cff8e10a8a4
jeremyhorwich <jeremyahorwich@gmail.com>
2024-02-06 10:31:43 -0600

    Update fileUpload.py

--- webAppLearning/fileUpload.py
+++ webAppLearning/fileUpload.py
@@ -1,8 +1,18 @@
-from flask import Flask, redirect, url_for, render_template, request
+import os
+from flask import Flask, redirect, url_for, render_template, request, flash
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "/tmp"
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 @app.route("/")
 @app.route("/index")
 def index():
     return "<p>Index</p>"
+
+@app.route("/upload", methods=["GET","POST"])
+def uploadFile():
+    if request.method == "GET":
+        return render_template("uploadFileForm.html")
