b1bda5b17e62946471bc52a38723c8293883b21c
jeremyhorwich <jeremyahorwich@gmail.com>
2024-02-09 14:40:33 -0600

    Update createFigure.py
    Refactored to avoid pyplot

--- webAppLearning/createFigure.py
+++ webAppLearning/createFigure.py
@@ -1,18 +1,19 @@
-import matplotlib.pyplot as plt
-from matplotlib import use as backendUse
+from  matplotlib.figure import Figure
+from matplotlib import use as setBackend
 from io import BytesIO
 import base64
 
-backendUse("agg")
+setBackend("agg")
 
 def createFigure():
     x = [1,2,3,4]
     y = [4,1,3,2]
-    plt.scatter(x,y)
+    fig = Figure()                  #Avoiding PyPlot because it's prone to memory leaks
+    ax = fig.subplots()
+    ax.scatter(x,y)
 
     stream = BytesIO()
-    plt.savefig(stream, format="png")
-    plt.close()                         #Without this we have memory leaks
+    fig.savefig(stream, format="png")
     figure = base64.b64encode(stream.getbuffer()).decode("ascii")
 
     return figure
