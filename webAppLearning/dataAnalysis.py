2bce969b96572633b1f737f45fec547ca935530b
jeremyhorwich <jeremyahorwich@gmail.com>
2024-02-13 16:25:20 -0600

    Update dataAnalysis.py
    Refactoring algorithm to clean it up a bit.
    
    Returning cost as well as theta now. Otherwise no behavior changes

--- webAppLearning/dataAnalysis.py
+++ webAppLearning/dataAnalysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def parseData(filepath): 
-    dataset =  pd.read_csv(filepath,usecols=['x','y']).values       #Will have to update if we want to handle different column names
+    dataset =  pd.read_csv(filepath,usecols=['x','y']).values       #TODO: Handle different column names
     return(dataset)
 
 def trainModel(data,learningRate,iterations):
@@ -11,16 +11,14 @@ def trainModel(data,learningRate,iterations):
     Y = data[:,1]
     Y = Y.reshape(Y.size,1)
     m = Y.shape[0]
-
     theta = np.zeros((2,1))
-    yHat = 0
-    cost = 0
-    dTheta = 0
+    
     for i in range(0,iterations):
-        yHat = np.dot(xPrime,theta)
-        cost = (1/(2*m))*np.sum(np.square(yHat - Y))
+        yHat = np.dot(xPrime,theta) 
 
-        d_theta = (1/m)*np.dot(xPrime.T, yHat - Y)
-        theta = theta - learningRate*d_theta
+        dTheta = (1/m)*np.dot(xPrime.T, yHat - Y)
+        theta = theta - learningRate*dTheta
+        if i == iterations:
+            cost = (1/(2*m))*np.sum(np.square(yHat - Y))
 
-    return theta
+    return theta, cost
