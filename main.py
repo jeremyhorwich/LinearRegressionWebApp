51c7ad1be1f31a0f065e3aed06cba2fff2a48471
jeremyhorwich <jeremyahorwich@gmail.com>
2024-04-14 11:07:54 -0500

    PEP8 main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request
-from createFigure import create_figure
-import dataAnalysis as da
+from figure import create_figure
+import analysis as da
 import os
 
 app = Flask(__name__)
@@ -13,72 +13,97 @@ def index():
     #Clean up the tmp folder from last time
     files = os.listdir(UPLOAD_FOLDER)
     for file in files:
-        filePath = os.path.join(UPLOAD_FOLDER, file)
-        if os.path.isfile(filePath):
-            os.remove(filePath)
-    return redirect("/upload")      #Just makes life a little easier
-    #return "<p>Index</p>"
+        file_path = os.path.join(UPLOAD_FOLDER, file)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+    return redirect("/upload")
+
 
 @app.route("/upload", methods=["GET","POST"])
-def uploadFile():
+def upload_file():
     if request.method == "GET":
         return render_template("uploadFile.html")
     if request.method == "POST":
-        if ("upload" not in request.files) or (request.files["upload"].filename == ""):
+        upload_not_found = "upload" not in request.files
+        upload_name_blank = request.files["upload"].filename == ""
+        if upload_not_found or upload_name_blank:
             return redirect("/fileUploadError")
         else:
             upload = request.files["upload"]
-            #Saving to tmp folder because files may be too big for session variables
-            upload.save(os.path.join(app.config["UPLOAD_FOLDER"], upload.filename))
-            return redirect(url_for("showFileVisualization", filename=upload.filename, errorReceived=False))
+            #Saving to tmp - files may be too big for session variables
+            dest = os.path.join(app.config["UPLOAD_FOLDER"], upload.filename)
+            upload.save(dest)
+            return redirect(url_for("show_file_visualization", 
+                                    filename=upload.filename, 
+                                    error_received=False))
+                
                 
 @app.route("/fileUploadError")
-def displayUploadError():
+def display_upload_error():
     return "<p>File Error</p>"
 
+
 @app.route("/fileUploadSuccess/", methods=["GET","POST"])
-def showFileVisualization():
+def show_file_visualization():
     if request.method == "GET":
         query = request.args.to_dict()
-        filename=query["filename"]
-        errorReceived=query["errorReceived"]
+        filename = query["filename"]
+        error_received = query["error_received"]
 
-        data = da.parseData(UPLOAD_FOLDER + "/" + filename)
+        data = da.parse_data(UPLOAD_FOLDER + "/" + filename)
         figure = create_figure(data)
-        if errorReceived == "True":         #Must check against string since we are getting errorReceived as a query parameter
+
+        #error_received is a query parameter, so we check against string
+        if error_received == "True":
             return render_template("scatterPlotWithError.html", image=figure)
         return render_template("scatterPlotDisplay.html", image=figure)
 
     if request.method == "POST":
         query = request.args.to_dict()
-        filename=query["filename"]
+        filename = query["filename"]
         
-        learningRate = request.form.get("learningRate",type=float)
+        learning_rate = request.form.get("learningRate",type=float)
         iterations = request.form.get("iterations",type=int)
 
-        if (learningRate is None) or (iterations is None):
-            return redirect(url_for("showFileVisualization", filename=filename, errorReceived=True))
-        if (learningRate < 0.00001) or (learningRate > 0.0001):
-            return redirect(url_for("showFileVisualization", filename=filename, errorReceived=True))
-        if (iterations < 50) or (iterations > 500):
-            return redirect(url_for("showFileVisualization", filename=filename, errorReceived=True))
+        if (learning_rate is None) or (iterations is None):
+            return redirect(url_for("show_file_visulaization", 
+                                    filename=filename, 
+                                    error_received=True))
         
-        return redirect(url_for("performLinearRegression",filename=filename,learningRate=learningRate,iterations=iterations))
+        if (learning_rate < 0.00001) or (learning_rate > 0.0001):
+            return redirect(url_for("show_file_visualization", 
+                                    filename=filename, 
+                                    error_received=True))
+        
+        if (iterations < 50) or (iterations > 500):
+            return redirect(url_for("show_file_visualization", 
+                                    filename=filename, 
+                                    errorReceived=True))
+        
+        return redirect(url_for("perform_linear_regression",
+                                filename=filename,
+                                learning_rate=learning_rate,
+                                iterations=iterations))
     
+
 @app.route("/fileUploadSuccess/linearRegression/", methods=["GET","POST"])
-def performLinearRegression():
+def perform_linear_regression():
     if request.method == "GET":
-        query=request.args.to_dict()
-        filename=query["filename"]
-        learningRate = float(query["learningRate"])
+        query = request.args.to_dict()
+        filename = query["filename"]
+        learning_rate = float(query["learning_rate"])
         iterations = int(query["iterations"])
 
-        data = da.parseData(UPLOAD_FOLDER + "/" + filename)     #TODO: Pass parsed data through? Doing this twice feels silly
-        theta, cost = da.trainModel(data,learningRate,iterations)
-        figure = create_figure(data,theta)                       #TODO: Doing this a second time feels silly, too
+        data = da.parse_data(UPLOAD_FOLDER + "/" + filename)            #TODO: Pass parsed data through? Doing this twice feels silly
+        theta, cost = da.train_model(data,learning_rate,iterations)
+        figure = create_figure(data,theta)                              #TODO: Doing this a second time feels silly, too
+        
         #Clean up the tmp folder
         if os.path.exists(UPLOAD_FOLDER + "/" + filename):
             os.remove(UPLOAD_FOLDER + "/" + filename)
-        return render_template("completedRegressionDisplay.html", image=figure, cost=cost)
+        return render_template("completedRegressionDisplay.html", 
+                               image=figure, 
+                               cost=cost)
+    
     if request.method == "POST":
         return redirect("/")
